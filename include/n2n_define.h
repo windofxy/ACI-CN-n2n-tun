e8514c4d8e7945b7779ce17fef85ef20de48d26c
Windofxy <52917206+windofxy@users.noreply.github.com>
2026-05-31 19:23:17 +0800

    修复wintun缓冲区过小的问题

--- include/n2n_define.h
+++ include/n2n_define.h
@@ -151,7 +151,7 @@ enum skip_add {SN_ADD = 0, SN_ADD_SKIP = 1, SN_ADD_ADDED = 2};
 #define N2N_EDGE_DEFAULT_CIDR_NM     24               /* ... also in cidr format  */
 
 #define N2N_SN_LPORT_DEFAULT 7654
-#define N2N_SN_PKTBUF_SIZE   2048
+#define N2N_SN_PKTBUF_SIZE   4096
 
 
 /* The way TUNTAP allocated IP. */
@@ -184,7 +184,7 @@ enum skip_add {SN_ADD = 0, SN_ADD_SKIP = 1, SN_ADD_ADDED = 2};
 #define N2N_MCAST_REG_COOKIE       0x00400000
 #define N2N_LOCAL_REG_COOKIE       0x01000000
 #define N2N_DESC_SIZE              16
-#define N2N_PKT_BUF_SIZE           2048
+#define N2N_PKT_BUF_SIZE           4096
 #define N2N_SOCKBUF_SIZE           64  /* string representation of INET or INET6 sockets */
 
 #define N2N_MULTICAST_PORT         1968
